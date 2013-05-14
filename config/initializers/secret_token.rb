7af11547b8a9c49e849843f02043af2f4f1323f5
Andy White <rawparadigm@gmail.com>
2013-05-14 15:12:48 -0500

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prayerbook::Application.config.secret_token = '7932bd3de56a6cdcc6d3fe7889492b2ae95a67e83c5d93162331c86a5b9b79c7ca62060d65aeecc7861e373309f0b59523a4d57893b181d55a4a3eb434f0a900'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Prayerbook::Application.config.session_store :cookie_store, key: '_prayerbook_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Prayerbook::Application.config.session_store :active_record_store
